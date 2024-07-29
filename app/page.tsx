c0c87508d4bf9b106c24a1b4acbe59460654a62f
sergei.klevtsov <sergei.klevtsov@accenture.com>
2024-07-30 01:42:13 +0300

    First approach to apply images

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import aiChallenge from '@/assets/images/1-AI-challenge.webp';
 
 export const metadata: Metadata = {
   title: 'Home | AI Rescue',
@@ -8,7 +9,15 @@ export default function Home() {
   return (
     <>
       <h1>Welcome to AI Rescue</h1>
-      {/* Остальной контент */}
+      <p>Здесь вы узнаете:</p>
+      <ol>
+        <li>
+          <img src={aiChallenge.src} alt="AI Challenge" width={200} height={200} />
+          Почему создание Super AI представляет самый серьёзный вызов человечеству за всю его историю</li>
+        <li>Почему, несмотря на этот вызов, люди будут стремиться создать его</li>
+        <li>О наихудших сценариях нанесения ущерба людям со стороны Super AI</li>
+        <li>О возможных подходах к устранению опасности от Super AI</li>
+      </ol>
     </>
   )
 }
