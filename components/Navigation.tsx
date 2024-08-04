7fcf0d4966bc8a858ce30434adc838d33de331a6
sergei.klevtsov <sergei.klevtsov@accenture.com>
2024-08-04 20:17:41 +0300

    Setting mobile menu

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,19 +1,42 @@
 'use client';
 
+import React from 'react';
 import Link from 'next/link'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import menuItems from '@/components/menuItems'
 
 const Navigation = () => {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(window.innerWidth > 798)
 
-  const LogoText = () => <p className="logo-text">/ <span style={{ fontWeight: 600 }}>Super AI Challenge</span> /</p>;
+  useEffect(() => {
+    const handleResize = () => {
+      setIsOpen(window.innerWidth > 798)
+    }
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
+  const LogoText = () => <p className="logo-text">/ <span style={{ fontWeight: 600 }}>Super-AI Challenge</span> /</p>;
+
+  const renderMenuItem = (item: string | (string | JSX.Element)[]) => {
+    if (Array.isArray(item)) {
+      return item.map((textPart, index) => (
+        <React.Fragment key={index}>
+          {textPart}
+          {index < item.length - 1 && <br />}
+        </React.Fragment>
+      ));
+    }
+    return item;
+  };
 
   return (
     <div className="md:max-w-[500px] sidebar-container">
       {/* Hamburger menu для мобильных устройств */}
       <div className="md:hidden p-4 flex justify-between items-center bg-[#333]">
-        <LogoText/>
+        <LogoText />
         <button
           onClick={() => setIsOpen(!isOpen)}
           className="text-white"
@@ -22,23 +45,22 @@ const Navigation = () => {
         </button>
       </div>
 
-      {/* Содержимое меню (как для мобильных, так и для десктопа) */}
-      {<aside className={`sidebar ${isOpen ? 'flex' : 'hidden'}`}>
+      <aside className={`sidebar ${isOpen ? 'flex' : 'hidden'}`}>
         <div className="sidebar-header">
-        <LogoText/>
+          <LogoText />
         </div>
-        <nav>
+        {isOpen && <nav>
           <ul>
             {menuItems.map((item) => (
               <li key={item.href}>
                 <Link href={item.href} onClick={() => setIsOpen(false)}>
-                  {item.text}
+                  {renderMenuItem(item.text)}
                 </Link>
               </li>
             ))}
           </ul>
-        </nav>
-      </aside>}
+        </nav>}
+      </aside>
     </div>
   )
 }
--- components/menuItems.js
+++ components/menuItems.js
@@ -1,8 +1,23 @@
 export default [
-  { href: '/', text: 'Home' },
-  { href: '/why-we-cant-give-up', text: 'Why We Can\'t Give Up on Creating Superintelligence' },
-  { href: '/scenarios', text: 'Scenarios of Events' },
-  { href: '/control-governance-alignment', text: 'Control, Governance or Alignment?' },
-  { href: '/rescue-approaches', text: 'Rescue approaches' },
-  { href: '/we-need-your-opinion', text: 'We need your opinion!' },
+  { href: "/", text: "Home" },
+  {
+    href: "/why-we-cant-give-up",
+    text: ["Why We Cannot Give Up", <>on Creating <span style={{color: 'var(--accent-color)', fontWeight: 700}}>Superintelligence</span></>],
+  },
+  { href: "/scenarios", text: "Scenarios of Events" },
+  {
+    href: "/control-governance-alignment",
+    text: "Control, Governance or Alignment?",
+  },
+  {
+    href: "/hal9000-vs-matrix",
+    text: [
+      <>
+        Why Hal-9000 Wanted to <span style={{ color: 'darkred', fontWeight: 700 }}>Kill All Humans</span>,
+      </>,
+      <>but the Matrix <span style={{ color: '#2a9146', fontWeight: 700 }}>Did Not</span></>,
+    ],
+  },
+  { href: "/rescue-approaches", text: "Rescue approaches" },
+  { href: "/we-need-your-opinion", text: "We need your opinion!" },
 ];
