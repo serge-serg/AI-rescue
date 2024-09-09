2ef2a855b68af260e0570c90e4505b7ee1940988
serge-serg <srgg67@gmail.com>
2024-09-10 00:10:31 +0300

    Changing iframe height

--- app/we-need-your-opinion/page.tsx
+++ app/we-need-your-opinion/page.tsx
@@ -6,7 +6,7 @@ import PageWrapper from '@/components/PageWrapper';
 const SurveyPage: React.FC = () => {
   return (
     <PageWrapper filename={__filename} underHeaderBlock={false}>
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSc446VlxyaXNtsgT0VfRyncjYg1kZyH6y9YIPAsAyrieQmTwQ/viewform?embedded=true" width="750" height="4570" style={{ marginLeft: '-32px'}} frameBorder="0" marginHeight={0} marginWidth={0}>Loading...</iframe>
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSc446VlxyaXNtsgT0VfRyncjYg1kZyH6y9YIPAsAyrieQmTwQ/viewform?embedded=true" width="750" height="5050" style={{ marginLeft: '-32px', maxWidth: '100%' }} frameBorder="0" marginHeight={0} marginWidth={0}>Loading...</iframe>
     </PageWrapper>
   );
 };
