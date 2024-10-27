fa574f7659ba10107f3b9952650d27b9a744af15
serge-serg <srgg67@gmail.com>
2024-10-27 10:15:12 +0200

    Fixing sys separator problem

--- utils/getPagePath.ts
+++ utils/getPagePath.ts
@@ -1,9 +1,18 @@
-import { join } from 'path';
+import { join, sep } from 'path';
 
 export function getPagePath(filename: string): string {
+  console.log('Filename received:', filename);
+  console.log('Current working directory:', process.cwd());
+  
   const relativePath = filename.replace(process.cwd(), '');
-  return '/' + relativePath
-    .split(join('app'))[1]
+  console.log('Relative path:', relativePath);
+
+  const path = '/' + relativePath
+    .split(sep + 'app')[1]
+    .replace(/\\page\.(js|ts)x?$/, '')
     .replace(/\/page\.(js|ts)x?$/, '')
     .replace(/^\//, '');
+
+  console.log('Generated path:', path);
+  return path;
 }
